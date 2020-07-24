f537c837509ca3967768808ea7b0bc7f5699832f
Denittka <dan.belokon@gmail.com>
2020-07-24 23:22:21 +0300

    Added the API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import api
 from data import db_session
 from flask import Flask, render_template
 
@@ -16,4 +17,6 @@ def index():
 
 if __name__ == "__main__":
     db_session.global_init("db/database.sqlite")
+    app.register_blueprint(api.api)
     app.run(host="127.0.0.1", port=8080)
+
